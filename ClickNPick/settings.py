2b30242802dae78fcacd10ac4ed847b687c5f1a6
AnilKumar-06 <anilmt06@gmail.com>
2025-04-01 21:33:47 +0530

    .

--- ClickNPick/settings.py
+++ ClickNPick/settings.py
@@ -150,5 +150,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anilmt06@gmail.com'
 EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
-#EMAIL_HOST_PASSWORD = "zynhwfmhmvpwsupy"
+
 EMAIL_USE_TLS = True
